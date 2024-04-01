f4864f75f7c374d52ee65ccc239241b4a6af0fe9
Ayush Jain <ayushjain2205@gmail.com>
2024-04-01 22:34:27 +0530

    Integrate thirdweb

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,15 @@
+import { client } from "@/utils/client";
+import { ConnectButton } from "thirdweb/react";
+
 export default function Layout({ children }) {
   return (
     <>
-      <div className="navbar bg-base-100">
-        <span className="text-primary font-bold">Eigen</span>
+      <div className="navbar bg-base-100 px-[20px]">
+        <div className="flex flex-row gap-[16px]">
+          <img className="h-[36px] w-[36px]" src="/icons/logo.svg" alt="" />
+          <span className="text-primary font-bold">Eigen</span>
+        </div>
+        <ConnectButton client={client} />
       </div>
       <div className="grow px-[40px] py-[20px]">{children}</div>
     </>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { client } from "@/utils/client";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
