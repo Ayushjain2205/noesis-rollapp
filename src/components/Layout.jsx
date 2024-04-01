5afb8cd5e45126e9c778f5558ec8430ead9657a4
Ayush Jain <ayushjain2205@gmail.com>
2024-04-02 00:48:21 +0530

    Configure top bar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,26 @@
 import { client } from "@/utils/client";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, lightTheme } from "thirdweb/react";
 
 export default function Layout({ children }) {
+  const customTheme = lightTheme({
+    colors: {
+      primaryButtonBg: "#9381FF",
+    },
+  });
   return (
     <>
-      <div className="navbar bg-base-100 px-[20px]">
+      <div className="navbar flex justify-between bg-base-100 px-[20px]">
         <div className="flex flex-row gap-[16px]">
           <img className="h-[36px] w-[36px]" src="/icons/logo.svg" alt="" />
           <span className="text-primary font-bold">Eigen</span>
         </div>
-        <ConnectButton client={client} />
+        <div className="flex flex-row gap-[100px]">
+          <div className="flex flex-row gap-[10px] items-center">
+            <img className="h-[36px] w-[36px]" src="/icons/coin.svg" alt="" />
+            <span className="font-bold text-[22px]">1200</span>
+          </div>
+          <ConnectButton theme={customTheme} client={client} />
+        </div>
       </div>
       <div className="grow px-[40px] py-[20px]">{children}</div>
     </>
