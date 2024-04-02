029a540f2cb33ed1b0b1d2d15e7392e219f48015
Ayush Jain <ayushjain2205@gmail.com>
2024-04-02 21:40:40 +0530

    Add navigation in  layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,7 @@
-import { client } from "@/utils/client";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { ConnectButton, lightTheme } from "thirdweb/react";
+import { client } from "../utils/client";
 
 export default function Layout({ children }) {
   const customTheme = lightTheme({
@@ -7,19 +9,57 @@ export default function Layout({ children }) {
       primaryButtonBg: "#9381FF",
     },
   });
+
+  const router = useRouter(); // Use the useRouter hook to get the current path
+
+  // A small function to determine if a link is active based on the current route
+  const isActive = (href) => router.pathname === href;
+
   return (
     <>
-      <div className="navbar flex justify-between bg-base-100 px-[20px]">
+      <div className="navbar flex items-center justify-between bg-base-100 px-[20px]">
         <div className="flex flex-row gap-[16px]">
           <img className="h-[36px] w-[36px]" src="/icons/logo.svg" alt="" />
-          <span className="text-primary font-bold">Eigen</span>
+          <span className="text-primary font-bold text-[36px]">Eigen</span>
+        </div>
+        <div className="flex flex-row gap-[40px]">
+          {/* Apply styles conditionally based on the active state */}
+          <Link href="/">
+            <span
+              className={`font-[500] text-[18px] ${
+                isActive("/") ? "text-primary" : "text-grey"
+              }`}
+            >
+              Datasets
+            </span>
+          </Link>
+          <Link href="/annotate">
+            <span
+              className={`font-[500] text-[18px] ${
+                isActive("/annotate") ? "text-primary" : "text-grey"
+              }`}
+            >
+              Annotate
+            </span>
+          </Link>
+          <Link href="/label">
+            <span
+              className={`font-[500] text-[18px] ${
+                isActive("/label") ? "text-primary" : "text-grey"
+              }`}
+            >
+              Label
+            </span>
+          </Link>
         </div>
         <div className="flex flex-row gap-[100px]">
           <div className="flex flex-row gap-[10px] items-center">
             <img className="h-[36px] w-[36px]" src="/icons/coin.svg" alt="" />
             <span className="font-bold text-[22px]">1200</span>
           </div>
-          <ConnectButton theme={customTheme} client={client} />
+          <div>
+            <ConnectButton theme={customTheme} client={client} />
+          </div>
         </div>
       </div>
       <div className="grow px-[40px] py-[20px]">{children}</div>
