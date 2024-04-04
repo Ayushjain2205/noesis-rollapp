f4ae3b08f9d5a851e7e3c37783c7683ba0c8b2c3
Ayush Jain <ayushjain2205@gmail.com>
2024-04-04 14:02:41 +0530

    Basic search bar componenet

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Layout from "@/components/Layout";
 import DatasetCard from "../components/UI/DatasetCard";
+import SearchBar from "../components/UI/SearchBar";
 
 export default function Home() {
   return (
     <Layout>
+      <SearchBar />
+      <br />
       <DatasetCard />
     </Layout>
   );
