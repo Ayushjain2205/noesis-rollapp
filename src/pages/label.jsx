446cc36482179c506215b8e128747a20f000f9fa
Ayush Jain <ayushjain2205@gmail.com>
2024-04-05 18:42:54 +0530

    Make images in anotator move on next and previous

--- src/pages/label.jsx
+++ src/pages/label.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import ImageAnnotator from "../components/Custom/ImageAnnotator";
 import toast from "react-hot-toast";
 import LabelCard from "../components/UI/LabelCard";
-import datasetDetails from "../utils/datasets";
+import datasetDetails from "../utils/datasets"; // Ensure this path matches your file structure
 
-const label = () => {
+const Label = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const images = [
+    "https://www.webbikeworld.com/wp-content/uploads/2014/03/Rear.jpg",
+    "https://img.freepik.com/premium-photo/motorcycles-cars-road-costa-smeralda-sardinia-island-italy-summer-motorcyclist-driving-scooter-highway-europe-view-moped-motorway_250132-18181.jpg",
+    "https://cdn.pixabay.com/video/2019/09/26/27260-362770008_tiny.jpg",
+    "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/11/highway-ap-1669716096.jpg",
+  ];
+
   const notify = () =>
     toast((t) => (
       <div className="flex flex-row items-center gap-[5px]">
         ✅ You earned <b>2 coins</b>
-        <img className="h-[25px]" src="/icons/coin.svg" alt="" />
+        <img className="h-[25px]" src="/icons/coin.svg" alt="coin" />
       </div>
     ));
+
+  const nextImage = () => {
+    setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setImageIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
 
   return (
     <Layout>
@@ -23,17 +41,17 @@ const label = () => {
               key={index}
               type={dataset.type}
               title={dataset.title}
-              fileCount={dataset.fileCount} // Assuming fileCount is now part of your dataset details
-              coins={dataset.coins} // Pass coins here, assuming it's a new or existing property of your datasets
+              fileCount={dataset.fileCount}
+              coins={dataset.coins}
             />
           ))}
         </div>
-        <div className="flex flex-col w-full ">
+        <div className="flex flex-col w-full">
           <div className="flex flex-row w-2/3 gap-[10px] items-center mb-[20px]">
-            <img className="h-[40px]" src="/icons/types/image.svg" alt="" />
+            <img className="h-[40px]" src="/icons/types/image.svg" alt="type" />
             <p className="text-[24px] font-[500]">My cute little dataset</p>
             <div className="flex flex-row gap-[5px] items-center ml-auto">
-              <img className="h-[40px]" src="/icons/coin.svg" alt="" />
+              <img className="h-[40px]" src="/icons/coin.svg" alt="coin" />
               <span className="text-[20px] font-[800]">2</span>
             </div>
           </div>
@@ -42,7 +60,7 @@ const label = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              className="stroke-primary shrink-0 w-6 h-6 primary"
+              className="stroke-primary shrink-0 w-6 h-6"
             >
               <path
                 strokeLinecap="round"
@@ -57,12 +75,12 @@ const label = () => {
             </div>
           </div>
           <div className="mt-[20px]">
-            <ImageAnnotator imageUrl="https://picsum.photos/400" />
+            <ImageAnnotator imageUrl={images[imageIndex]} />
           </div>
 
-          <div className="flex flex-row  mt-[550px] ml-[150px] w-[500px] justify-between">
-            <button className="">
-              <img className="h-[60px]" src="/icons/arrow.svg" alt="" />
+          <div className="flex flex-row mt-[550px] ml-[150px] w-[500px] justify-between">
+            <button onClick={prevImage}>
+              <img className="h-[60px]" src="/icons/arrow.svg" alt="Previous" />
             </button>
             <button
               className="h-[60px] text-[20px] w-[150px] bg-green-500 bg-opacity-70 font-[500] rounded-xl"
@@ -70,11 +88,11 @@ const label = () => {
             >
               Submit
             </button>
-            <button className="">
+            <button onClick={nextImage}>
               <img
                 className="h-[60px] rotate-180"
                 src="/icons/arrow.svg"
-                alt=""
+                alt="Next"
               />
             </button>
           </div>
@@ -84,4 +102,4 @@ const label = () => {
   );
 };
 
-export default label;
+export default Label;
